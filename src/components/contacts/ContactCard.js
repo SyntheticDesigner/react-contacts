6df5f76603d1aa9926488da40280ecb27eb6ed83
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-01-30 17:07:49 -0800

    styles: created style option with bootstrap

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -1,12 +1,17 @@
-import {
-  ProfilePic,
-  Contact,
-  Profile,
-  Pic,
-  Info,
-  Button,
-  ButtonContainer,
-} from "../../styles/Styled";
+// import {
+//   ProfilePic,
+//   Contact,
+//   Profile,
+//   Pic,
+//   Info,
+//   Button,
+//   ButtonContainer,
+// } from "../../styles/Styled";
+
+import BSContainer from "react-bootstrap/Container";
+import BSButton from "react-bootstrap/Button";
+import BSImage from "react-bootstrap/Image";
+import { Col as BSCol, Row as BSRow } from "react-bootstrap";
 
 const ContactCard = ({
   firstName = "",
@@ -15,25 +20,33 @@ const ContactCard = ({
   profilePic = "",
 }) => {
   return (
-    <Contact>
-      <Profile>
-        <Pic>
-          <ProfilePic
+    <BSContainer className='m-4 shadow-sm'>
+      <BSRow>
+        <BSCol xs={3}>
+          <BSImage
+            style={{ width: "5.2em" }}
+            roundedCircle={true}
             src={profilePic}
             alt={`Profile of ${firstName} ${lastName}.`}
           />
-        </Pic>
-        <Info>
-          <p style={{ margin: ".2em", width: "100%" }}>
-            {firstName} {lastName}
-          </p>
-          <p style={{ margin: ".2em" }}>{phoneNumber}</p>
-        </Info>
-      </Profile>
-      <ButtonContainer>
-        <Button>{`\u2716`}</Button>
-      </ButtonContainer>
-    </Contact>
+        </BSCol>
+        <BSCol xs={7} className='border-start d-flex align-items-center'>
+          <div>
+            <p style={{ margin: "0" }}>
+              {firstName} {lastName}
+            </p>
+            <p>{phoneNumber}</p>
+          </div>
+        </BSCol>
+        <BSCol xs={2} className='d-flex align-items-center'>
+          <BSButton
+            className='rounded-circle h-5 w-3'
+            variant='light'
+            size='sm'
+          >{`\u2716`}</BSButton>
+        </BSCol>
+      </BSRow>
+    </BSContainer>
   );
 };
 
